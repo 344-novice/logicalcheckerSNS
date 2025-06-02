66a352bf906b376b102896f1c08240178b11fba3
344-novice <miyoshiwataru@miyoshiwatarunoMacBook-Air.local>
2025-06-02 20:36:36 +0900

    Add Laravel default files

--- routes/web.php
+++ routes/web.php
@@ -3,56 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function() {
-    return view('login');
-});
-Route::post('/', function() {
-    return view('login');
-});
-
-Route::get('/signin', function() {
-    return view('signin');
-});
-Route::post('/signin', function() {
-    return view('signin');
-});
-
-Route::get('/home', function() {
-    return view('home');
-});
-Route::post('/home', function() {
-    return view('home');
-});
-
-Route::get('/user', function() {
-    return view('user');
-});
-Route::post('/user', function() {
-    return view('user');
-});
-
-Route::get('/admin/login', function() {
-    return view('admin/login');
-});
-Route::post('/admin/login', function() {
-    return view('admin/login');
-});
-
-Route::get('/admin/top', function() {
-    return view('admin/top');
-});
-Route::post('/admin/top', function() {
-    return view('admin/top');
-});
-
-
-// post/detailにすると404エラーになる
-Route::get('/post', function() {
-    return view('post_detail');
-});
-Route::post('/post', function() {
-    return view('post_detail');
-});
+Route::get('/', function () {
+    return Inertia::render('welcome');
+})->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
