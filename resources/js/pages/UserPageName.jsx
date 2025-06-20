a605eca426e7f4c190f4c59ff67e43e9221376c3
344-novice <344literature@gmail.com>
2025-06-20 17:45:16 +0900

    自分のツイートからユーザー詳細に遷移するとマイページとしてレンダリングする処理

--- resources/js/pages/UserPageName.jsx
+++ resources/js/pages/UserPageName.jsx
@@ -1,11 +1,10 @@
 import { useEffect, useState } from "react";
 
-export default function UserPageName() {
+export default function UserPageName({ id, loginUserId }) {
     const [userPageName, setUserPageName] = useState("User");
 
     useEffect(() => {
-        // ToDo: loginUserIdとパス名が一致したときも分岐に追加
-        if (location.pathname === "/mypage") {
+        if (location.pathname === "/mypage" || id === loginUserId) {
             setUserPageName("MyPage");
         }
     }, []);
